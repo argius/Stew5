17e4f62a5b9acedf787d7005a00ba16123e3ef8e
argius <argius.net@gmail.com>
2017-05-11 00:29:24 +0900

    Update XmlExporterTest.java to fix a timezone problem

--- src/test/java/stew5/io/XmlExporterTest.java
+++ src/test/java/stew5/io/XmlExporterTest.java
@@ -10,13 +10,14 @@ public final class XmlExporterTest {
     @Test
     public void testAddRow() throws IOException {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        Date time = new Date(5000000 * 1000L);
         try (XmlExporter exporter = new XmlExporter(bos)) {
             exporter.addHeader(a("name", "number"));
             exporter.addRow(a("test1", "567"));
             exporter.addRow(a("test2", "890 "));
             exporter.addRow(a("nulltest", null));
             exporter.addRow(a("objecttest", new StringBuilder("test")));
-            exporter.addRow(a("timetest", new Date(5000000 * 1000L)));
+            exporter.addRow(a("timetest", time));
         }
         assertEquals("<?xml version=\"1.0\" encoding=\"utf-8\"?>%n"
                      + "<!DOCTYPE table SYSTEM \"stew-table.dtd\">%n"
@@ -29,7 +30,9 @@ public final class XmlExporterTest {
                      + "ACED0005737200176A6176612E6C616E672E537472696E674275696C6465723CD5FB145A4C6ACB03000078707704000"
                      + "00004757200025B43B02666B0E25D84AC02000078700000001400740065007300740000000000000000000000000000"
                      + "00000000000000000000000000000000000078</object></row>%n"
-                     + "<row><string>timetest</string><time display=\"Sat Feb 28 05:53:20 JST 1970\">5000000000</time></row>%n"
+                     + "<row><string>timetest</string><time display=\""
+                     + time
+                     + "\">5000000000</time></row>%n"
                      + "</table>",
                      bos.toString().replaceAll("\r?\n", "%n"));
     }
