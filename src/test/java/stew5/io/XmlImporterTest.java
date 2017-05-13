c334624baec9d36cec667d43f270a819364510c4
argius <argius.net@gmail.com>
2017-05-14 01:53:45 +0900

    Fix broken test in XmlImporterTest

--- src/test/java/stew5/io/XmlImporterTest.java
+++ src/test/java/stew5/io/XmlImporterTest.java
@@ -5,9 +5,9 @@ import static stew5.TestUtils.*;
 import java.io.*;
 import java.sql.*;
 import java.util.*;
+import javax.xml.datatype.*;
 import javax.xml.namespace.*;
 import javax.xml.stream.*;
-import org.joda.time.*;
 import org.junit.*;
 import org.junit.rules.*;
 
@@ -20,27 +20,37 @@ public final class XmlImporterTest {
 
     @Test
     public void testXmlImporter() throws Exception {
-        final String sql = "select * from " + TBL + " order by id";
+        final String sql = "select id, content, updated_at, txt from " + TBL + " order by id";
         try (Connection conn = connection(); Statement stmt = conn.createStatement()) {
             prepareTable(conn);
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
             try (ResultSet rs = stmt.executeQuery(sql); XmlExporter exp = new XmlExporter(bos)) {
-                exp.addHeader(new String[]{"1x", "2", "3y", "4", "5z"});
+                exp.addHeader(new String[]{"1x", "2", "3y", "4z"});
                 while (rs.next()) {
                     List<Object> a = new ArrayList<>();
-                    for (int i = 0; i < 5; i++) {
+                    for (int i = 0; i < 4; i++) {
                         a.add(rs.getObject(i + 1));
                     }
                     exp.addRow(a.toArray());
                 }
             }
             try (XmlImporter imp = new XmlImporter(new ByteArrayInputStream(bos.toByteArray()))) {
-                assertEquals("[1x, 2, 3y, 4, 5z]", Arrays.deepToString(imp.readHeader()));
-                String expected1 = "[1, abcde123, " + ts(1200000000000L) + ", LOB1]";
-                assertEquals(expected1, Arrays.toString(imp.nextRow()));
-                String expected2 = "[2, fgh456, " + ts(1300000000000L) + ", LOB2]";
-                assertEquals(expected2, Arrays.toString(imp.nextRow()));
-                assertEquals(0, imp.nextRow().length);
+                assertEquals("[1x, 2, 3y, 4z]", Arrays.deepToString(imp.readHeader()));
+                Object[] row;
+                // first row
+                row = imp.nextRow();
+                assertEquals(4, row.length);
+                assertEquals(1L, row[0]);
+                assertEquals("abcde123", row[1]);
+                assertEquals(1200000000000L, ((XMLGregorianCalendar)row[2]).toGregorianCalendar().getTimeInMillis());
+                assertEquals("LOB1", row[3]);
+                // second row
+                row = imp.nextRow();
+                assertEquals(4, row.length);
+                assertEquals(2L, row[0]);
+                assertEquals("fgh456", row[1]);
+                assertEquals(1300000000000L, ((XMLGregorianCalendar)row[2]).toGregorianCalendar().getTimeInMillis());
+                assertEquals("LOB2", row[3]);
             }
         }
     }
@@ -105,11 +115,6 @@ public final class XmlImporterTest {
         thrown.expectMessage("unexpected event: ");
         XMLEventFactory ef = XMLEventFactory.newInstance();
         XmlImporter.isElementNameEquals(ef.createComment(""), "test");
-    }
-
-    static String ts(long epochMillis) {
-        DateTime dt = new DateTime(epochMillis);
-        return dt.toString("yyyy-MM-dd'T'HH:mm:ssZZ");
     }
 
     static void prepareTable(Connection conn) throws SQLException {
